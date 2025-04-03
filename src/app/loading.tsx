89b902f02dd92cafd5852995fd1830eb9c0296e6
NeilRow56 <neilrowland56@gmail.com>
2025-04-03 14:30:45 +0100

    Added Animated loading spinner and not found components

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Spinner from '@/components/shared/spinner'
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='flex min-h-screen w-full items-center justify-center'>
+      <Spinner />
+    </div>
+  )
+}
